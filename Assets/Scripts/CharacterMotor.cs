58c39a2e3d35f2e77eaa26b95fc073a8f555b68d
aydenlimyip <65572042+aydenlimyip@users.noreply.github.com>
2021-07-28 13:20:25 +1200

    horizontal movement

---
+++ Assets/Scripts/CharacterMotor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMotor : MonoBehaviour
+{
+    public CharacterController controller;
+    private float MovementSpeed = 0.5f;
+    private Vector3 Velo;
+    private bool BoolJump;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Velo.x = Input.GetAxis("Horizontal") * MovementSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            BoolJump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(Velo);
+        BoolJump = false;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        
+    }
+}
