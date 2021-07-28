2f93e54f52e70eb1c26c76eb5188b2056a025bc8
Stv13579 <48304098+Stv13579@users.noreply.github.com>
2021-07-28 13:40:01 +1200

    Added toggleable light source as a prefab
    Added toggleable light source as a prefab

---
+++ Assets/Scripts/LightScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    private Light spotLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spotLight = transform.GetChild(0).gameObject.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            LightToggle();
+            Debug.Log("Test");
+        }
+    }
+
+    void LightToggle()
+    {
+        spotLight.enabled = !spotLight.enabled;
+    }
+}
