f5bbd206d116c91b4735f60e08a9e8baec58e2da
aydenlimyip <65572042+aydenlimyip@users.noreply.github.com>
2021-07-28 14:55:45 +1200

    character not done

--- Assets/Scripts/CharacterMotor.cs
+++ Assets/Scripts/CharacterMotor.cs
@@ -4,29 +4,68 @@ using UnityEngine;
 
 public class CharacterMotor : MonoBehaviour
 {
-    public CharacterController controller;
-    private float MovementSpeed = 0.5f;
-    private Vector3 Velo;
-    private bool BoolJump;
+    public Rigidbody2D RB2D;
+    private float MovementSpeed = 10.0f;
+    private Vector2 Velo;
+    private bool Grounded = true;
+    private float JumpHeight = 5000.0f;
+    private bool Chain = false;
 
     // Update is called once per frame
     void Update()
     {
+        // movement on the horizontal axis
         Velo.x = Input.GetAxis("Horizontal") * MovementSpeed;
-        if (Input.GetButtonDown("Jump"))
+
+        // flipping the sprite
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0) // spirte looking at the left side
         {
-            BoolJump = true;
+            characterScale.x = -10;
+        }
+        if (Input.GetAxis("Horizontal") > 0) // spirte looking at the right side
+        {
+            characterScale.x = 10;
+        }
+        transform.localScale = characterScale;
+
+
+        if(Chain == true) // if player is in chain then it can climb up
+        {
+            Velo.y = Input.GetAxis("Vertical") * MovementSpeed;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && Grounded)
+        {
+            RB2D.velocity = Vector2.up * JumpHeight;
+            Grounded = false;
         }
     }
 
     void FixedUpdate()
     {
-        controller.Move(Velo);
-        BoolJump = false;
+        RB2D.velocity = new Vector2 (Velo.x,Velo.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            Grounded = true;
+        }
     }
 
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Chain")
+        {
+            Chain = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Chain")
+        {
+            Chain = false;
+        }
     }
 }
