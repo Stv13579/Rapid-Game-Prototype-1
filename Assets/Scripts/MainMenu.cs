9852e1e785c9cc7361a9591e78382caf5812f801
Ayden <aydenjarrodlimyip@gmail.com>
2021-07-30 21:16:42 +1200

    Main Menu and end scene done

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Transform OptionsPanel;
+    public GameObject PausePanel;
+    public void PlayGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+
+    }
+    public void Reset()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        PausePanel.SetActive(true);
+        OptionsPanel.GetChild(2).gameObject.SetActive(false);
+    }
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+        PausePanel.SetActive(false);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+}
